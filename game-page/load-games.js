a6f2d5c32db92e4e2628a28abfe2b4c31a845083
agostonicholas <12q567@gmail.com>
2025-04-09 20:41:27 -0400

    made the game grid work, json and java script for dynamic game additions. did some css and stuff for styling.

--- game-page/load-games.js
+++ game-page/load-games.js
@@ -1,13 +1,13 @@
-fetch("games.json")
+fetch("../game-page/games.json")
   .then(response => response.json())
   .then(games => {
     games.forEach(game => {
       const gameCard = document.createElement("div");
       gameCard.classList.add("game-card");
+      let imageHTML = game.image ? `<a href= ${game.link}><img src="${game.image}" alt="${game.name}"height= 200px width= 150px></a>` : "";
       gameCard.innerHTML = `
-        <img src="${game.image}" alt="${game.name}">
+        ${imageHTML}
         <h3>${game.name}</h3>
-        <p>${game.description}</p>
         <a href="${game.link}" class="play-button">Play Now</a>
       `;
       document.querySelector(".game-grid").appendChild(gameCard);
