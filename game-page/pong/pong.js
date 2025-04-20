860cdea6be5a4950a60fe006240ecf868653edee
agostonicholas <12q567@gmail.com>
2025-04-19 21:12:21 -0400

    The ball BOUNCES BOIS LETS GOOO

--- game-page/pong/pong.js
+++ game-page/pong/pong.js
@@ -6,7 +6,24 @@ const cWidth = canvas.width; // canvas width
 const cHeight = canvas.height; // canvas height
 const keys = {}; // keys object to store key strokes
 
-// game objects
+// game classes
+/* 
+class scoreBoard{
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = white;
+        this.score = 0;
+        }
+    draw() {
+
+    }
+}
+
+*/ 
+
 class paddlePlayer {
     constructor(x, y, width, height) {
         this.x = x;
@@ -69,42 +86,41 @@ class ball {
         this.width = width;
         this.height = height;
         this.color = 'white';
-        this.speed = 5;
-        this.velocityY = 5;
+        this.speedX = 5;
+        this.speedY = 5;
     }
     initialize() {
         this.x = cWidth / 2 - this.width / 2; 
         this.y = cHeight / 2 - this.height / 2;
-        this.speed = 5;
+        Math.random() < 0.5 ? this.speedX = -this.speedX : this.speedX = this.speedX;
+        Math.random() < 0.5 ? this.speedY = -this.speedY : this.speedY = this.speedY;
     }
     draw() {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
     move() {
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x += this.speedX;
+        this.y += this.speedY;
     }
     collision(paddlePlayer, paddleCPU) {
         if (this.x < paddlePlayer.x + paddlePlayer.width && this.x + this.width > paddlePlayer.x && this.y < paddlePlayer.y + paddlePlayer.height && this.y + this.height > paddlePlayer.y) {
-            this.speed = -this.speed;
+            this.speedY = -this.speedY;
+            this.speedX = -this.speedX;
         }
         if (this.x < paddleCPU.x + paddleCPU.width && this.x + this.width > paddleCPU.x && this.y < paddleCPU.y + paddleCPU.height && this.y + this.height > paddleCPU.y) {
-            this.speed = -this.speed;
+            this.speedY = -this.speedY;
+            this.speedX = -this.speedX;
         }
     }
     collisionWall(canvasHeight){
-        if (this.y <= 0) {
-            this.y = 0;
-            this.velocityY *= -1;
-          } 
-          if (this.y + this.size >= canvasHeight) {
-            this.y = canvasHeight - this.size;
-            this.velocityY *= -1;
-          }
+        if (this.y + this.height > canvasHeight || this.y < 0) {
+            this.speedY = -this.speedY;
+        }
     }
 }
 
+// keyboard checks
 window.addEventListener('keydown', (e) => {
     keys[e.key] = true; // set key to true when pressed
     if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", " "].includes(e.key)) {
@@ -115,15 +131,20 @@ window.addEventListener('keyup', (e) => {
     keys[e.key] = false; // set key to true when pressed
 });
 
+// game objects
 const gameBall = new ball(400, 300, 10, 10);
 const player = new paddlePlayer(50, 250, 10, 100);
 const cpu = new paddleCPU(740, 250, 10, 100);
+const playerGoal = new goal(0, 0, 5, 800);
+const cpuGoal = new goal(600, 0, 5, 800);
 
+
+//game loop
 function gameLoop() {
-
+        // clear 
     ctx.clearRect(0, 0, cWidth, cHeight);
 
-
+       // update
     // player movement
     if(keys['ArrowUp']) {
         player.moveUp();
@@ -141,7 +162,8 @@ function gameLoop() {
         gameBall.speed = -gameBall.speed;
     }
     gameBall.collisionWall(cHeight);
-    
+
+        // redraw
     gameBall.draw();
     player.draw();
     cpu.draw();
