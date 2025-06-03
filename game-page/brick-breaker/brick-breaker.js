1dec754cf8b68d3f53b2aef4ff650bc5407c1196
Nicholas Agosto <12q567@gmail.com>
2025-06-02 20:01:29 -0400

    got the ball to bounce off the bricks properly

--- game-page/brick-breaker/brick-breaker.js
+++ game-page/brick-breaker/brick-breaker.js
@@ -5,6 +5,40 @@ const ctx = canvas.getContext('2d');
 const cWidth = canvas.width; // canvas width
 const cHeight = canvas.height; // canvas height
 const keys = {}; // keys object to store key strokes
+const brickColors = ['blue', 'red', 'green', 'yellow', 'orange']
+
+class scoreboard{
+    constructor(x, y, score){
+        
+    }
+}
+
+
+class brick {
+    constructor(x, y, colorIndex){
+        this.x = x;
+        this.y = y;
+        this.length = 30;
+        this.width = 10;
+        this.colorIndex = colorIndex;
+        this.color = brickColors[this.colorIndex]
+    }
+    
+    draw(){
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.length, this.width);
+    }
+    
+    downgradeColor() {
+        this.colorIndex--;
+        if (this.colorIndex >= 0) {
+            this.color = brickColors[this.colorIndex];
+        }
+    }
+
+}
+
+
 
 class paddle {
     constructor(x, y, width, height) {
@@ -77,6 +111,24 @@ class ball {
                 this.y = paddlePlayer.y - this.height - 1;
         }
     }
+
+    collisionBrick(brick) {
+        if (this.x < brick.x + brick.width && 
+            this.x + this.width > brick.x && 
+            this.y < brick.y + brick.height && 
+            this.y + this.height > brick.y) {
+
+                let hitPos = ((this.x + this.width / 2) - (brick.x + brick.width / 2)) / (brick.width / 2);
+                let maxBounce = Math.PI / 3;
+                let angle = hitPos * maxBounce;
+                let speed = Math.sqrt(this.speedX ** 2 + this.speedY ** 2) || 5;
+
+                this.speedX = speed * Math.sin(angle); // Ensure ball goes right
+                this.speedY = -Math.abs(speed * Math.cos(angle));
+
+                this.y = brick.y - this.height - 1;
+        }
+    }
     
     collisionWall(canvasHeight, canvasWidth){
         if (this.y + this.height > canvasHeight || this.y < 0) {
@@ -103,6 +155,7 @@ gameBall = new ball(450, 450, 10, 10);
 let ballIsReady = true;
 let ballsLeft = 3;
 youLose = false;
+score = 0;
 
 gameBall.initialize(paddlePlayer);
 
@@ -110,6 +163,27 @@ function gameOver(){
     if(youLose = true){
         ctx.clearRect(0, 0, cWidth, cHeight);
         gameLoop();
+    }
+}
+
+
+
+let brickRows = 5;
+let brickCols = 8;
+let brickWidth = 80;
+let brickHeight = 30;
+let brickPadding = 10;
+let brickOffsetTop = 50;
+let brickOffsetLeft = 35;
+
+let bricksArray = [];
+
+for (let row = 0; row < brickRows; row++){
+    for (let col = 0; col < brickCols; col++){
+        let colorIndex = Math.floor(Math.random() * brickColors.length);
+        let x = brickOffsetLeft + col * (brickWidth + brickPadding);
+        let y = brickOffsetTop + row * (brickHeight + brickPadding);
+        bricksArray.push(new brick(x, y, colorIndex));
     }
 }
 
@@ -139,6 +213,31 @@ function gameLoop() {
         gameBall.y += gameBall.speedY;
         gameBall.collision(paddlePlayer);
         gameBall.collisionWall(cHeight, cWidth);
+
+        for (let i = bricksArray.length - 1; i >= 0; i--) {
+            let b = bricksArray[i];
+            gameBall.collisionBrick(b);
+
+            if (
+                gameBall.x < b.x + b.length &&
+                gameBall.x + gameBall.width > b.x &&
+                gameBall.y < b.y + b.width &&
+                gameBall.y + gameBall.height > b.y
+                ) {
+                if(b.colorIndex > 0){
+                    b.downgradeColor();
+                    gameBall.speedY = -gameBall.speedY;
+                    score += 10;
+                } else {
+                    bricksArray.splice(i, 1);
+                    gameBall.speedY = -gameBall.speedY;
+                    score += 10;
+                }
+            }
+        }
+        if(bricksArray.length == 0){
+            ballIsReady = true
+        }
     }
 
     if(gameBall.y > cHeight){
@@ -146,9 +245,14 @@ function gameLoop() {
         ballIsReady = true;
     }
 
+    
+
     if (keys['ArrowLeft']) paddlePlayer.moveLeft();
     if (keys['ArrowRight']) paddlePlayer.moveRight(cWidth);
         //render
+    for (let b of bricksArray){
+        b.draw();
+    }
     paddlePlayer.draw();
     gameBall.draw();
 
