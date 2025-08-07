6a019cf03961be9b1fc06acab9995d08740488eb
agostonicholas <12q567@gmail.com>
2025-08-07 16:09:59 -0400

    adding in the backend functions and login stuff

---
+++ backend-functions.js
@@ -0,0 +1,28 @@
+export async function saveScore(game, user, score){
+    try {
+        const response = await fetch('https://nicks-games-backend.onrender.com/saveScore', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ game, user, score })
+        });
+    const data = await response.json();
+    console.log('Score saved:', data);
+    } catch (error) {
+        console.error('Error saving score:', error);
+    }
+}
+
+export async function getLeaderboard(){
+    try {
+        const response = await fetch('https://nicks-games-backend.onrender.com/getLeaderboard', {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'},
+        });
+        const data = await response.json();
+        console.log('Leaderboard data: ', data);
+        return data;
+    } catch (error) {
+        console.error('Error fetching leaderboard: ', error);
+        return [];
+    }
+}
